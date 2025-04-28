a54652a91482c1c5b46f6e67d4ad254d2a2575c2
mohamedna3eem <168696045+mohamedna3eem@users.noreply.github.com>
2025-04-29 00:01:34 +0300

    Revert "signup_validation"

--- lib/features/auth/data/repos/auth_repo_impl.dart
+++ lib/features/auth/data/repos/auth_repo_impl.dart
@@ -6,6 +6,8 @@ import 'package:pharma_now/features/auth/domain/repo/auth_repo.dart';
 import 'package:pharma_now/features/auth/domain/repo/entities/user_entity.dart';
 
 import '../../../../core/services/firebase_auth_service.dart';
+import '../entities/RegisterResponseEntity.dart';
+import 'entities/RegisterResponseEntity.dart';
 
 class AuthRepoImpl extends AuthRepo {
   final FirebaseAuthService firebaseAuthService;
@@ -13,7 +15,7 @@ class AuthRepoImpl extends AuthRepo {
   AuthRepoImpl({required this.firebaseAuthService});
 
   @override
-  Future<Either<Failures, UserEntity>> createUserWithEmailAndPassword(
+  Future<Either<Failures, RegisterResponseEntity>> createUserWithEmailAndPassword(
       String email, String password, String name) async {
     try {
       var user = await firebaseAuthService.createUserWithEmailAndPassword(
---
+++ lib/features/auth/domain/entities/RegisterResponseEntity.dart
@@ -0,0 +1,10 @@
+class RegisterResponseEntity {
+  RegisterResponseEntity({
+      this.message,this.statusMessage,});
+
+  String? message;
+  String? statusMessage;
+
+
+
+}
